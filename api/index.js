bbcd0fd5dfbfb57bf1f0cf30099f26553ab5e14a
jmobispo <jmobispo123@gmail.com>
2024-07-18 20:58:31 +0800

    initial commit

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res)=> res.send('EXPRESS on VERCEL'));
+
+app.listen(3000, ()=>{
+    console.log(`RUNNING ON 3000`)
+});
+
+module.exports = app;
