0871b51317998e322fd760d06a1faf7a32223f4e
jmobispo <jmobispo123@gmail.com>
2024-07-20 00:23:02 +0800

    added update, fetch by id, api and fixed responses

--- api/index.js
+++ api/index.js
@@ -15,6 +15,32 @@ connectDB()
 app.get('/', (req, res)=> res.send('EXPRESS on VERCEL'));
 
 
+//transactions
+app.get(
+    '/api/transactions/all',
+    async (request, response) => {
+        try {
+            const transactions = await Transaction.find()
+            response.status(200).json(transactions)
+        } catch (error) {
+            response.status(500).json({code: "ERR50000", message: error.message, data: null})
+        }
+    }
+)
+
+app.get(
+    '/api/transactions/:id',
+    async (request, response) => {
+        try {
+            const { id } = request.params
+            const transaction = await Transaction.findById(id)
+            response.status(200).json(transaction)
+        } catch (error) {
+            response.status(500).json({code: "ERR50000", message: error.message, data: null})
+        }
+    }
+)
+
 app.post(
     '/api/transactions/create', 
     async (request, response) => {
@@ -22,7 +48,39 @@ app.post(
             const transaction = await Transaction.create(request.body)
             response.status(200).json(transaction)
         } catch (error) {
-            response.status(500).json({message: error.message})
+            response.status(500).json({code: "ERR50000", message: error.message, data: null})
+        }
+    }
+)
+
+app.put(
+    '/api/transactions/update/:id',
+    async (request, response) => {
+        try {
+            const { id } = request.params
+            const transaction = await Transaction.findByIdAndUpdate(id, request.body)
+            
+            if(!transaction){
+                return response.status(404).json({code: "ERR40004", message: "Transaction not found!", data: null})
+            }
+            const updatedTransaction = await Transaction.findById(id)
+            response.status(200).json({code: "SUC20000", message: "Updated Successfully", data: updatedTransaction})
+
+        } catch (error) {
+            response.status(500).json({code: "ERR50000", message: error.message, data: null})
+        }
+    }
+)
+
+//merchants
+app.get(
+    '/api/merchants/all',
+    async (request, response) => {
+        try {
+            const merchants = await Merchant.find()
+            response.status(200).json({code: "SUC20000", message: "Fetched Successfully!", data: merchants})
+        } catch (error) {
+            response.status(500).json({code: "ERR50000", message: error.message, data: null})
         }
     }
 )
@@ -37,20 +95,7 @@ app.post(
             console.log(merchant)
             response.status(200).json(merchant)
         } catch (error) {
-            response.status(500).json({message: error.message})
-        }
-    }
-)
-
-app.get(
-    '/api/merchants/all',
-    async (request, response) => {
-        try {
-            const merchants = await Merchant.find()
-            console.log(merchants)
-            response.json(merchants)
-        } catch (error) {
-            response.status(500).json({message: error.message})
+            response.status(500).json({code: "ERR50000", message: error.message, data: null})
         }
     }
 )
